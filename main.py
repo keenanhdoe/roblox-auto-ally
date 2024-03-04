a0fbdf50c23f05b6e306a8f2335bdbe861f54bb4
r3dd <161536510+keenanhdoe@users.noreply.github.com>
2024-03-04 10:05:36 -0600

    minimized, added threading main.py

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
+Certainly! I've modified the code to take the number of threads as input. Now you can specify the number of threads when running the script.
+
+```python
 import time
 import requests
 import traceback
+import threading
 
 COOKIES = ["_|WARNING:-DO-NOT-SHARE-THIS.--Sharing-this-will-allow-someone-to-log-in-as-you-and-to-steal-your-ROBUX-and-items.|"]
 WEBHOOK = ""
 GROUP_ID = 0
+
 class Bypass:
     def __init__(self, robloxCookie):
         self.cookie = robloxCookie
@@ -20,18 +25,15 @@ class Bypass:
         assert set_cookie_header, "An error occurred while getting the set_cookie"
         return set_cookie_header.split(".ROBLOSECURITY=")[1].split(";")[0]
 
-        
     def get_rbx_authentication_ticket(self):
         response = requests.post("https://auth.roblox.com/v1/authentication-ticket", headers={"rbxauthenticationnegotiation":"1", "referer": "https://www.roblox.com/camel", 'Content-Type': 'application/json', "x-csrf-token": self.xcsrf_token}, cookies={".ROBLOSECURITY": self.cookie})
         assert response.headers.get("rbx-authentication-ticket"), "An error occurred while getting the rbx-authentication-ticket"
         return response.headers.get("rbx-authentication-ticket")
         
-        
     def get_csrf_token(self) -> str:
         response = requests.post("https://auth.roblox.com/v2/logout", cookies = {".ROBLOSECURITY": self.cookie})
         xcsrf_token = response.headers.get("x-csrf-token")
         assert xcsrf_token, "An error occurred while getting the X-CSRF-TOKEN. Could be due to an invalid Roblox Cookie"
-                 
         return xcsrf_token
     
 class RobloxCookie:
@@ -52,10 +54,11 @@ class RobloxCookie:
         return self._x_token
 
 class RobloxAllySender:
-    def __init__(self, roblox_cookie):
+    def __init__(self, roblox_cookie, num_threads):
         self.cookie = roblox_cookie
         self.current_cursor = ''
         self.already_added = []
+        self.num_threads = num_threads
 
     def scrape_assets(self):
         items =  requests.get(f"https://catalog.roblox.com/v1/search/items?category=Clothing&limit=120&salesTypeFilter=1&sortType=3&subcategory=ClassicShirts&cursor={self.current_cursor}", 
@@ -114,28 +117,13 @@ class RobloxAllySender:
                 allies += self.get_allies_group(group)
           except Exception as e:
               requests.post(WEBHOOK, json={"content": f"ERROR: {traceback.format_exc()}"})
-    
-class iterator:
-    def __init__(self, iterable):
-        self.iterable = iterable
-        self.length = len(iterable)
-        self.index = 0
 
-    def __iter__(self):
-        return self
+def start_ally_sender(num_threads):
+    ally_sender = RobloxAllySender(iterator([RobloxCookie(Bypass(cookie).start_process()) for cookie in COOKIES]), num_threads)
+    ally_sender.start_process()
 
-    def __next__(self):
-        if self.index < self.length:
-            self.index += 1
-        else:
-            self.index = 0
+# Take the number of threads as input
+num_threads = int(input("Enter the number of threads: "))
 
-    def get_current(self):
-        if self.index < self.length:
-            return self.iterable[self.index]
-        else:
-            self.index = 0
-            return self.iterable[0]
-
-ally_sender = RobloxAllySender(iterator([RobloxCookie(Bypass(cookie).start_process()) for cookie in COOKIES]))
-ally_sender.start_process()
+# Start threading
+threading.Thread(target=start_ally_sender, args=(num_threads,)).start()
